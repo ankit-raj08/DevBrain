05640dd070ad2bdb793c440186230f7369b56684
Ankit <ankitrj787@gmail.com>
2026-03-16 01:16:32 +0530

    fix vercel cors

--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ const connectDB = require("./config/db");
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5175", "https://dev-brain-coral.vercel.app"],
+    origin: [
+        "http://localhost:5175",
+        /https:\/\/dev-brain.*\.vercel\.app$/
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true
 }));
